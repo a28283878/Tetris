f27daf7f8d3001efd622601afdca0d2169e45087
Toby Chen <atch84@gmail.com>
2017-01-08 04:07:10 +0800

    feat hold

---
+++ src/view/HoldBoard.java
@@ -0,0 +1,47 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import model.Shape;
+import model.Shape.Tetrominoes;
+
+public class HoldBoard extends Board{
+
+	public HoldBoard() {
+		super();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g, false);
+		if (!curPiece.getShape().equals(Tetrominoes.NoShape)) {
+			for (int i = 0; i < 4; ++i) {
+				int x = curX - curPiece.x(i);
+				int y = curY - curPiece.y(i);
+				drawSquare(g,(int) (30 + x * squareWidth()),(int) (50 + y * squareHeight()),
+						curPiece.getShape());
+			}
+		}
+	}
+	
+	@Override
+	int squareHeight() {
+		// TODO Auto-generated method stub
+		return (int) getSize().getWidth() / 5;
+	}
+	
+	@Override
+	int squareWidth() {
+		// TODO Auto-generated method stub
+		return (int) getSize().getHeight() / 5;
+	}
+	
+	public Tetrominoes hold(Shape shape) {
+		Tetrominoes temp = curPiece.getShape();
+		curPiece.setShape(shape.getShape());
+		repaint();
+		return temp;
+	}
+	
+}
